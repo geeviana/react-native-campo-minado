7d520dda9e4ca89e4f51a80277b7ae08c72dd4eb
Geraldo Viana <geefatec@gmail.com>
2019-03-19 14:43:50 -0300

    Commit inicial do projeto

---
+++ src/components/Field.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import params from '../params'
+
+export default props =>{
+    const styleField = [styles.field]
+    // outros estilos aqui
+    if(styleField.length === 1 ) styleField.push(styles.regular)
+
+    return(
+        <View style={styleField}>
+
+        </View>
+    )
+} 
+
+const styles = StyleSheet.create({
+    field:{
+        height: params.blockSize,
+        width: params.blockSize,
+    }
+})
